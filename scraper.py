25032d94fdf28c6f096bc48788731ac042027a26
Liam Ellis <liamhellis@gmail.com>
2017-10-31 13:17:28 -0400

    reinit web scraper is functioning

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import sys
+import pathlib
+from selenium import webdriver
+from PIL import Image
+from io import BytesIO
+
+url = "https://unsplash.com/search/photos/" + sys.argv[1] if len(sys.argv) > 1 else "https://unsplash.com"
+result_folder = "./images-" + sys.argv[1] + "/" if len(sys.argv) > 1 else "./images/"
+scroll = (int(sys.argv[2]) if sys.argv[2].isdigit() else 1000) if len(sys.argv) > 2 else 1000
+selector = "#gridMulti img"
+driver = webdriver.Firefox()
+
+driver.get(url)
+driver.execute_script("window.scrollTo(0,"+str(scroll)+");")
+time.sleep(5)
+image_elements = driver.find_elements_by_css_selector("#gridMulti img")
+pathlib.Path(result_folder).mkdir(parents=True, exist_ok=True)
+
+for image_element in image_elements:
+    image_url = image_element.get_attribute("src").split("?")[0]
+    image_object = requests.get(image_url)
+    image_name = image_url.split("/")[-1]
+    image = Image.open(BytesIO(image_object.content))
+    image.save(result_folder + image_name + "." + image.format, image.format)
+
+driver.quit()
